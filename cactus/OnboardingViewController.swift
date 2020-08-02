82eb425fa4ae07125a1240e7fa54c914ddab622d
ulzhanai <bissarinova@gmail.com>
2020-08-03 01:08:03 +0600

    commited to the new branch minimum task of hw 4 and extra tasks of hw 2 and 3

---
+++ cactus/OnboardingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  OnboardingViewController.swift
+//  cactus
+//
+//  Created by Ulzhan Bissarinova on 8/2/20.
+//  Copyright © 2020 Ulzhan Bissarinova. All rights reserved.
+//
+
+import Foundation
